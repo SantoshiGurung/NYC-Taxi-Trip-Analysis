5bc662cc07d1167438ea7c4201302780dbda2a61
Santoshi Gurung <santoseegrg25@gmail.com>
2026-05-12 19:38:57 +0545

    Complete taxi analysis logic

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -25,3 +25,23 @@ SELECT
 FROM Trip_Speeds
 GROUP BY hour_of_day
 ORDER BY avg_speed ASC;
+
+-- QUERY 3: PAYMENT METHOD INFLUENCE
+-- Comparing average tips for Credit Card vs. Cash to see which is more profitable.
+SELECT 
+    payment_type,
+    COUNT(*) AS total_trips,
+    AVG(tip_amount) AS avg_tip_amount
+FROM taxi_trips
+WHERE fare_amount > 0
+GROUP BY payment_type
+ORDER BY avg_tip_amount DESC;
+
+-- QUERY 4: PEAK REVENUE HOURS
+-- Identifying which hour of the day generates the most total revenue.
+SELECT 
+    EXTRACT(HOUR FROM pickup_datetime) AS trip_hour,
+    SUM(total_fare) AS total_revenue
+FROM taxi_trips
+GROUP BY trip_hour
+ORDER BY total_revenue DESC;
